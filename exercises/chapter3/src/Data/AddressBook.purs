f712eceb0b76f320f320f5e159897e46d24655af
Mirichan <michael@mroz.io>
2017-03-03 19:17:23 +1100

    Completed chapter 3

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -1,10 +1,9 @@
 module Data.AddressBook where
 
 import Prelude
-
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
-import Data.Maybe (Maybe)
+import Data.List (List(..), filter, head, nubBy)
+import Data.Maybe (Maybe, isJust)
 
 type Address =
   { street :: String
@@ -32,8 +31,29 @@ emptyBook = empty
 insertEntry :: Entry -> AddressBook -> AddressBook
 insertEntry = Cons
 
+-- I assume that if I look at the docs for List, I'll find this exact function.
+-- I didn't do that because I was instructed to reuse code from the previous
+-- implementation, which it seemed to me that the book intended me to organically
+-- discover `find`.
+find :: forall a. (a -> Boolean) -> List a -> Maybe a
+find f = head <<< filter f
+
 findEntry :: String -> String -> AddressBook -> Maybe Entry
-findEntry firstName lastName = head <<< filter filterEntry
-  where
-  filterEntry :: Entry -> Boolean
-  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+findEntry firstName lastName = find (\entry -> entry.firstName == firstName
+                                            && entry.lastName  == lastName)
+
+-- Ideally, I'd implement a proper equality check here, but it seems likely that
+-- the book will bring that up itself sooner or later
+findEntryByAddress :: Address -> AddressBook -> Maybe Entry
+findEntryByAddress addr = find (\entry -> addr.street == entry.address.street
+                                       && addr.city   == entry.address.city
+                                       && addr.state  == entry.address.state)
+
+-- The book suggests that I look up how to check if a list is empty.
+-- I didn't do that, because I like code reuse. I'll look into it another time.
+exists :: String -> String -> AddressBook -> Boolean
+exists firstName lastName = isJust <<< findEntry firstName lastName
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubBy (\e1 e2 -> e1.firstName == e2.firstName
+                                 && e1.lastName  == e2.lastName)
